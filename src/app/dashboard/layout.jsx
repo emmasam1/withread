6806e89fe8d23022a23c15a239e0d6fd74d8f0ee
emmanuel sam <fndm4@gmail.com>
2025-06-07 15:18:04 +0100

    added sign up page

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -9,7 +9,17 @@ import {
   LeftOutlined,
   RightOutlined,
 } from "@ant-design/icons";
-import { Button, Layout, Menu, theme } from "antd";
+import {
+  Button,
+  Layout,
+  Menu,
+  theme,
+  Input,
+  Modal,
+  Form,
+  Checkbox,
+} from "antd";
+import Image from "next/image";
 
 const { Header, Sider, Content } = Layout;
 
@@ -20,6 +30,18 @@ export default function DashboardLayout({ children }) {
   } = theme.useToken();
 
   const siderWidth = collapsed ? 80 : 200;
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
 
   return (
     <div className="h-screen overflow-hidden">
@@ -102,6 +124,7 @@ export default function DashboardLayout({ children }) {
       >
         {/* Fixed Header */}
         <Header
+          className="flex justify-between !items-center !px-5"
           style={{
             position: "fixed",
             top: 0,
@@ -112,7 +135,42 @@ export default function DashboardLayout({ children }) {
             padding: 0,
             background: colorBgContainer,
           }}
-        />
+        >
+          <div>
+            <Input
+              placeholder="Search anything..."
+              className="mt-4 !rounded-full !pr-12"
+              suffix={
+                <Image
+                  src="/images/search-normal.png"
+                  alt="search icon"
+                  width={20}
+                  height={20}
+                />
+              }
+            />
+          </div>
+          <div className="flex items-center gap-2.5">
+            <Button
+              className="!bg-black !text-[#D9D9D9] !border-0 !rounded-full !py-4 !px-4 flex gap-2"
+              onClick={showModal}
+            >
+              {/* <Image src="/images/add.png" width={20} height={20} alt="icon" /> */}
+              Login
+            </Button>
+            <div className="bg-[#F3F3F4] rounded-full p-2">
+              <Image src="/images/sms.png" width={20} height={20} alt="icon" />
+            </div>
+            <div className="bg-[#F3F3F4] rounded-full p-2">
+              <Image
+                src="/images/notification-bing.png"
+                width={20}
+                height={20}
+                alt="icon"
+              />
+            </div>
+          </div>
+        </Header>
 
         {/* Scrollable Content */}
         <Content
@@ -126,6 +184,93 @@ export default function DashboardLayout({ children }) {
           {children}
         </Content>
       </div>
+
+      <Modal
+        // title="Basic Modal"
+        closable={{ "aria-label": "Custom Close Button" }}
+        open={isModalOpen}
+        footer={false}
+        className="!rounded-0"
+        width={400}
+      >
+        <div className="flex justify-center">
+          <Image
+            src="/images/login_logo.png"
+            alt="logo"
+            width={150}
+            height={150}
+          />
+        </div>
+        <h1 className="font-semibold text-center text-2xl mt-3 mb-5">
+          Welcome Back to Withread!
+        </h1>
+
+        <Form
+          name="basic"
+          layout="vertical" // 👈 This moves labels above the inputs
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Email Address Or Username"
+            name="username"
+            className="!mb-3"
+            rules={[
+              {
+                required: true,
+                message: "Please input your email or username!",
+              },
+            ]}
+          >
+            <Input placeholder="Enter your email/username" />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            className="!mb-2"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <div className="flex justify-between items-center">
+            <Form.Item name="remember" valuePropName="checked">
+              <Checkbox>Keep me logged in</Checkbox>
+            </Form.Item>
+
+            <Link href="#" className="!text-gray-400 -mt-6">Forget Password?</Link>
+          </div>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="!bg-[#141823] !rounded-full !w-full !py-5"
+            >
+              Log in now
+            </Button>
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="default"
+              className="!bg-transparent !border !border-black !rounded-full !w-full !py-5 flex items-center justify-center"
+            >
+              <Image
+                src="/images/google.png"
+                alt="logo"
+                width={150}
+                height={150}
+                className="mr-2"
+              />
+            </Button>
+          </Form.Item>
+        </Form>
+        <p className="text-center">Don’t have an account? <Link href='/signup'>Sign Up</Link></p>
+      </Modal>
     </div>
   );
 }
---
+++ src/app/signup/page.jsx
@@ -0,0 +1,181 @@
+"use client";
+
+import { Form, Input, Button, Checkbox, Row, Col } from "antd";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Page = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-[2fr_600px]">
+      {/* Left Column */}
+      <div className="p-4 bg-white h-screen relative overflow-y-auto">
+        <Image
+          src="/images/Group.png"
+          alt="icon"
+          width={100}
+          height={100}
+          className="absolute top-0 left-0"
+        />
+        <Image
+          src="/images/shape-99.png"
+          alt="icon"
+          width={100}
+          height={100}
+          className="absolute bottom-0 right-0"
+        />
+
+        <div className="flex justify-center items-center flex-col p-10">
+          <div className="flex justify-center">
+            <Image
+              src="/images/login_logo.png"
+              alt="logo"
+              width={150}
+              height={150}
+            />
+          </div>
+          <h1 className="font-semibold text-center text-black text-2xl mt-3 mb-5">
+            Welcome to Withread!
+          </h1>
+
+          <Form
+            name="register"
+            layout="vertical"
+            style={{ maxWidth: 600 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            {/* First Name and Last Name side by side */}
+            <Row gutter={16}>
+              <Col span={12}>
+                <Form.Item
+                  label="First Name"
+                  name="firstName"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please enter your first name!",
+                    },
+                  ]}
+                >
+                  <Input placeholder="First Name" />
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item
+                  label="Last Name"
+                  name="lastName"
+                  rules={[
+                    { required: true, message: "Please enter your last name!" },
+                  ]}
+                >
+                  <Input placeholder="Last Name" />
+                </Form.Item>
+              </Col>
+            </Row>
+
+            {/* Other fields */}
+            <Form.Item
+              label="Email Address"
+              name="email"
+              rules={[
+                { required: true, message: "Please enter your email address!" },
+              ]}
+            >
+              <Input placeholder="Enter your email" />
+            </Form.Item>
+
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[{ required: true, message: "Please enter a username!" }]}
+            >
+              <Input placeholder="Enter your username" />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please enter your password!" },
+                {
+                  min: 8,
+                  message: "Password must be at least 8 characters long.",
+                },
+              ]}
+            >
+              <Input.Password placeholder="Enter password" />
+            </Form.Item>
+
+            {/* <Form.Item
+              label="Confirm Password"
+              name="confirmPassword"
+              dependencies={["password"]}
+              rules={[
+                { required: true, message: "Please confirm your password!" },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue("password") === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject("Passwords do not match!");
+                  },
+                }),
+              ]}
+            >
+              <Input.Password placeholder="Confirm password" />
+            </Form.Item> */}
+
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="!bg-[#141823] !rounded-full !w-full !py-5 mt-4"
+              >
+                Sign up now
+              </Button>
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                type="default"
+                className="!bg-transparent !border !border-black !rounded-full !w-full !py-5 flex items-center justify-center"
+              >
+                <Image
+                  src="/images/google.png"
+                  alt="logo"
+                  width={150}
+                  height={150}
+                  className="mr-2"
+                />
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+
+      {/* Right Column */}
+      <div
+        className="hidden md:block p-4 h-screen"
+        style={{
+          backgroundImage: "url('/images/bg-img.png')",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default Page;
